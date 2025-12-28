646e8407190d12152123bdec3f96eb248637f2d0
zxal20 <106362015+zxal20@users.noreply.github.com>
2025-12-28 20:35:53 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Service Worker – Final Fixed
    =============================== */
 
-const CACHE_NAME = 'magicplayer-cache-v99999999999999999999999';
+const CACHE_NAME = 'magicplayer-cache-v99999999999999999999999999';
 
 /* صفحه fallback برای آفلاین */
 const OFFLINE_PAGE = '/offline.html';
