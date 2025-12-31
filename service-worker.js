c39c7cfd74aa61b02eacc8e6556d3520733875b0
zxal20 <106362015+zxal20@users.noreply.github.com>
2025-12-31 05:16:53 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Service Worker – Final Fixed
    =============================== */
 
-const CACHE_NAME = 'magicplayer-cache-v9999999999';
+const CACHE_NAME = 'magicplayer-cache-v9999999999999';
 
 /* صفحه fallback برای آفلاین */
 const OFFLINE_PAGE = '/offline.html';
@@ -19,8 +19,8 @@ const ASSETS = [
 
   '/css/Vazirmatn-font-face.css',
 
-  '/js/babylon.js',
-  '/js/babylonjs.loaders.min.js',
+  '/js/three.module.js',
+  
 
   '/js/mediapipe/selfie_segmentation.js',
   '/js/mediapipe/selfie_segmentation.binarypb',
