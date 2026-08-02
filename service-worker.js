30a0eecd386eb72eb236f131cc0aa668ef73f26f
zxal20 <106362015+zxal20@users.noreply.github.com>
2026-08-02 07:23:09 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Service Worker – Bulletproof Offline Fix
    =============================== */
 
-const CACHE_NAME = 'magicplayer-cache-v119';
+const CACHE_NAME = 'magicplayer-cache-v199';
 
 const ASSETS = [
     './',
